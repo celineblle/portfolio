5a8547f40c529a07fc513da88259bdacf718e507
celineblle <celine.baille97@gmail.com>
2025-08-27 19:58:26 +0200

    insersion d'element dans column

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,6 +1,29 @@
+import { allStacks } from "./elements/stacksAndProjets";
 
 function Column() {
-  return <div></div>;
+
+
+
+  return <div>
+    <h2>Développeuse full stack Java, Angular, React</h2>
+    {allStacks.map((stack) => (
+      <ul>
+        <li>{stack.name}</li>
+        {stack.projects.map((projet) => (
+          <ul>
+            <li>
+              {projet.name}
+            </li>
+            {projet.description.map((description) => (
+              <li>
+                {description}
+              </li>
+            ))}
+          </ul>
+        ))}
+      </ul>
+    ))}
+  </div>;
 }
 
 export default Column;
